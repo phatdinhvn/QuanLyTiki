deeb3b4fc4dc8b87b1a2f3c71aaddbbdfc214cc6
Phat Dinh <phatdinhvn@outlook.com>
2018-05-02 01:21:44 +0700

    get value from a website

---
+++ BangGia/BangGia/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BangGia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.butGet = new System.Windows.Forms.Button();
+            this.colTenSanPham = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colTenSanPham,
+            this.colGia});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 331);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // butGet
+            // 
+            this.butGet.Location = new System.Drawing.Point(120, 41);
+            this.butGet.Name = "butGet";
+            this.butGet.Size = new System.Drawing.Size(75, 23);
+            this.butGet.TabIndex = 1;
+            this.butGet.Text = "Get";
+            this.butGet.UseVisualStyleBackColor = true;
+            this.butGet.Click += new System.EventHandler(this.butGet_Click);
+            // 
+            // colTenSanPham
+            // 
+            this.colTenSanPham.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.colTenSanPham.HeaderText = "Tên sản phẩm";
+            this.colTenSanPham.Name = "colTenSanPham";
+            this.colTenSanPham.ReadOnly = true;
+            // 
+            // colGia
+            // 
+            this.colGia.HeaderText = "Giá";
+            this.colGia.Name = "colGia";
+            this.colGia.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butGet);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button butGet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTenSanPham;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colGia;
+    }
+}
+
---
+++ BangGia/BangGia/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace BangGia
+{
+    public partial class Form1 : Form
+    {
+        string HTML;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns[1].DefaultCellStyle.Format = "c0";
+            dataGridView1.Columns[1].DefaultCellStyle.FormatProvider = CultureInfo.GetCultureInfo("vi-VN", "VND");
+        }
+
+        private void butGet_Click(object sender, EventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                HTML = wc.DownloadString("https://tiki.vn/laptop-may-vi-tinh/c1846");
+            }
+
+            //textBox1.Text = HTML;
+
+            MatchCollection m1 = Regex.Matches(HTML, @"data-title=""\s*(.+?)\s*""", RegexOptions.Singleline);
+
+            MatchCollection m2 = Regex.Matches(HTML, @"<span class=""final-price"">\s*(.+?)\s* ₫</span>", RegexOptions.Singleline);
+
+            string[] tenSP = new string[m1.Count];
+            for (int i = 0; i<m1.Count;i++)
+            {
+                string name = m1[i].Groups[1].Value;
+                int gia = int.Parse(m2[i].Groups[1].Value.Replace(".",""));
+                dataGridView1.Rows.Add(name, gia);
+            }
+        }
+    }
+}
