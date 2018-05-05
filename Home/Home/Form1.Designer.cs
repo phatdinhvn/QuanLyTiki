071e9d2105084afc6055ca926413745a8a5e14a0
Phat Dinh <phatdinhvn@outlook.com>
2018-05-05 23:18:42 +0700

    MainForm

---
+++ Home/Home/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Home
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.butExit = new System.Windows.Forms.Button();
+            this.butMinimized = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.but1 = new System.Windows.Forms.Button();
+            this.panelMark = new System.Windows.Forms.Panel();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.butSearch = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(184)))), ((int)(((byte)(241)))));
+            this.panel2.Controls.Add(this.butSearch);
+            this.panel2.Controls.Add(this.txtSearch);
+            this.panel2.Controls.Add(this.butMinimized);
+            this.panel2.Controls.Add(this.butExit);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 108);
+            this.panel2.TabIndex = 0;
+            // 
+            // butExit
+            // 
+            this.butExit.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.butExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.butExit.FlatAppearance.BorderSize = 0;
+            this.butExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butExit.Location = new System.Drawing.Point(765, 0);
+            this.butExit.Name = "butExit";
+            this.butExit.Size = new System.Drawing.Size(35, 34);
+            this.butExit.TabIndex = 1;
+            this.butExit.Text = "X";
+            this.butExit.UseVisualStyleBackColor = false;
+            this.butExit.Click += new System.EventHandler(this.butExit_Click);
+            // 
+            // butMinimized
+            // 
+            this.butMinimized.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.butMinimized.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.butMinimized.FlatAppearance.BorderSize = 0;
+            this.butMinimized.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butMinimized.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butMinimized.Location = new System.Drawing.Point(727, 0);
+            this.butMinimized.Name = "butMinimized";
+            this.butMinimized.Size = new System.Drawing.Size(35, 34);
+            this.butMinimized.TabIndex = 1;
+            this.butMinimized.Text = "_";
+            this.butMinimized.UseVisualStyleBackColor = false;
+            this.butMinimized.Click += new System.EventHandler(this.butMinimized_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.panelMark);
+            this.panel3.Controls.Add(this.but4);
+            this.panel3.Controls.Add(this.but3);
+            this.panel3.Controls.Add(this.but2);
+            this.panel3.Controls.Add(this.but1);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(150, 450);
+            this.panel3.TabIndex = 1;
+            // 
+            // but1
+            // 
+            this.but1.FlatAppearance.BorderSize = 0;
+            this.but1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(184)))), ((int)(((byte)(241)))));
+            this.but1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.but1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but1.Location = new System.Drawing.Point(0, 108);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(150, 34);
+            this.but1.TabIndex = 1;
+            this.but1.Text = "Điện Thoại - Máy Tính Bảng";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.but1_Click);
+            // 
+            // panelMark
+            // 
+            this.panelMark.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(184)))), ((int)(((byte)(241)))));
+            this.panelMark.Location = new System.Drawing.Point(145, 74);
+            this.panelMark.Name = "panelMark";
+            this.panelMark.Size = new System.Drawing.Size(10, 34);
+            this.panelMark.TabIndex = 2;
+            // 
+            // but2
+            // 
+            this.but2.FlatAppearance.BorderSize = 0;
+            this.but2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(184)))), ((int)(((byte)(241)))));
+            this.but2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.but2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but2.Location = new System.Drawing.Point(0, 142);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(150, 34);
+            this.but2.TabIndex = 3;
+            this.but2.Text = "Laptop - Thiết bị IT";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.but2_Click);
+            // 
+            // but3
+            // 
+            this.but3.FlatAppearance.BorderSize = 0;
+            this.but3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(184)))), ((int)(((byte)(241)))));
+            this.but3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.but3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but3.Location = new System.Drawing.Point(0, 176);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(150, 34);
+            this.but3.TabIndex = 4;
+            this.but3.Text = "Máy ảnh - Quay phim";
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.but3_Click);
+            // 
+            // but4
+            // 
+            this.but4.FlatAppearance.BorderSize = 0;
+            this.but4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(184)))), ((int)(((byte)(241)))));
+            this.but4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.but4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but4.Location = new System.Drawing.Point(0, 210);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(150, 34);
+            this.but4.TabIndex = 4;
+            this.but4.Text = "Sách, VPP, Quà tặng";
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.but4_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(218, 78);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(349, 30);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
+            // 
+            // butSearch
+            // 
+            this.butSearch.BackColor = System.Drawing.Color.Gainsboro;
+            this.butSearch.FlatAppearance.BorderSize = 0;
+            this.butSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butSearch.Location = new System.Drawing.Point(567, 78);
+            this.butSearch.Name = "butSearch";
+            this.butSearch.Size = new System.Drawing.Size(87, 30);
+            this.butSearch.TabIndex = 3;
+            this.butSearch.Text = "Tìm kiếm";
+            this.butSearch.UseVisualStyleBackColor = false;
+            this.butSearch.Click += new System.EventHandler(this.butSearch_Click);
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(157, 115);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(631, 323);
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(410, 142);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 272);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button butExit;
+        private System.Windows.Forms.Button butMinimized;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Panel panelMark;
+        private System.Windows.Forms.Button butSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Home/Home/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Home
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void butExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn muốn thoát chương trình?","",MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void butMinimized_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void but1_Click(object sender, EventArgs e)
+        {
+            panelMark.Top = but1.Top;
+            panelMark.Height = but1.Height;
+            getStringHTML("https://tiki.vn/dien-thoai-may-tinh-bang/c1789");
+        }
+
+        private void but2_Click(object sender, EventArgs e)
+        {
+            panelMark.Top = but2.Top;
+            panelMark.Height = but2.Height;
+            getStringHTML("https://tiki.vn/laptop-may-vi-tinh/c1846");
+        }
+
+        private void but3_Click(object sender, EventArgs e)
+        {
+            panelMark.Top = but3.Top;
+            panelMark.Height = but3.Height;
+            getStringHTML("https://tiki.vn/may-anh/c1801");
+
+
+        }
+
+        private void but4_Click(object sender, EventArgs e)
+        {
+            panelMark.Top = but4.Top;
+            panelMark.Height = but4.Height;
+            loadListView(getStringHTML("https://tiki.vn/nha-sach-tiki/c8322"));
+        }
+
+        private void butSearch_Click(object sender, EventArgs e)
+        {
+            getStringHTML(@"https://tiki.vn/search?q=" + txtSearch.Text);
+        }
+
+        private string getStringHTML(string search) {
+            string HTML;
+            using (WebClient wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                HTML = wc.DownloadString(search.Replace(" ", "+"));
+            }
+            return HTML;
+        }
+
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //textBox1.Text = getStringHTML(@"https://tiki.vn/search?q=" + txtSearch.Text); //////////////////////////
+            }
+        }
+
+        private void loadListView(string HTML) {
+
+            WebRequest request;
+            string s = @"class=""product - image img - responsive"" src=""(.+?)"" alt";
+            MatchCollection m1 = Regex.Matches(HTML, s, RegexOptions.Singleline);
+
+            //MatchCollection m2 = Regex.Matches(HTML, @"<span class=""final-price"">\s*(.+?)\s* ₫</span>", RegexOptions.Singleline);
+
+            //string[] tenSP = new string[m1.Count];
+            for (int i = 0; i < m1.Count; i++)
+            {
+                //request = WebRequest.Create(m1[i].Groups[1].Value);
+                //using (var res = request.GetResponse())
+                //using (var str = res.GetResponseStream())
+                //{
+                //    imageList1.Images.Add(Bitmap.FromStream(str));
+                //    ListViewItem item = new ListViewItem();
+                //    item.Text = "test";
+                //    item.ImageIndex = 0;
+                //    listView.Items.Add(item);
+                //}
+                textBox1.Text = m1[i].Groups[1].Value + "\n";
+                //int gia = int.Parse(m2[i].Groups[1].Value.Replace(".", ""));
+            }
+        }
+
+    }
+}
